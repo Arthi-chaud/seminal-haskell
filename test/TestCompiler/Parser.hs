e39d0cb47d35e221d6b88b8ca3f7dc4da76fa2a3
Arthur Jamet <arthur.jamet@gmail.com>
2023-06-15 11:30:57 +0100

    Fix tests

--- test/TestCompiler/Parser.hs
+++ test/TestCompiler/Parser.hs
@@ -1,15 +1,15 @@
 module TestCompiler.Parser (testSuite) where
 
-import Seminal.Compiler.Parser
 import Seminal.Compiler.Runner
-import Test.HUnit ((@=?), assertFailure)
+import Test.HUnit ((@=?), assertFailure, assertBool)
 import Test.Framework (Test, testGroup, buildTest)
 import Test.Framework.Providers.HUnit (testCase)
-import GHC (moduleNameString, moduleName, ms_mod, pm_mod_summary)
+import GHC (moduleNameString, moduleName, ms_mod, pm_mod_summary, ParsedModule)
 import System.Directory (getPermissions, setOwnerReadable, setPermissions)
+import Data.List (isPrefixOf, isSuffixOf, isInfixOf)
 
-runParser :: [FilePath] -> IO ParsingResult
-runParser files = runCompiler files (parseFiles files)
+runParser :: [FilePath] -> IO (Either String [(FilePath, ParsedModule)])
+runParser files = runCompiler files return
 
 testSuite :: Test
 testSuite = testGroup "Compiler's Parser" [
@@ -26,19 +26,21 @@ testSuite = testGroup "Compiler's Parser" [
     buildTest $ do
         res <- runParser ["test/assets/invalid/syntax-error.hs"]
         return $ testCase "Invalid Parsing (Syntax Error)" $ case res of
-            Left [(_, e)] -> e @=? SyntaxError
+            Left errMsg -> assertBool "" $ "parse error" `isPrefixOf` errMsg
             _ -> assertFailure "Parsing should have failed",
     buildTest $ do
         res <- runParser ["idonotexist"]
         return $ testCase "Invalid Parsing (File Not Found)" $ case res of
-            Left [(_, e)] -> e @=? FileNotFound
+            Left errMsg -> assertBool "" $
+                "target" `isPrefixOf` errMsg &&
+                "is not a module name or a source file" `isSuffixOf` errMsg
             _ -> assertFailure "Parsing should have failed",
     buildTest $ do
         let filePath = "test/assets/valid/explicit-main-module.hs"
         _ <- getPermissions filePath >>= setPermissions filePath . setOwnerReadable False
         res <- runParser [filePath]
         _ <- getPermissions filePath >>= setPermissions filePath . setOwnerReadable True
-        return $ testCase "Invalid Parsing (Pemrission denied)" $ case res of
-            Left [(_, e)] -> e @=? PermissionDenied
+        return $ testCase "Invalid Parsing (Permission denied)" $ case res of
+            Left errMsg -> assertBool "" $ "permission denied" `isInfixOf` errMsg
             _ -> assertFailure "Parsing should have failed"
     ]
--- test/TestCompiler/TypeChecker.hs
+++ test/TestCompiler/TypeChecker.hs
@@ -1,7 +1,6 @@
 module TestCompiler.TypeChecker (testSuite) where
 
-import Seminal.Compiler.Parser
-import Test.HUnit ((@=?))
+import Test.HUnit ((@=?), assertFailure)
 import Test.Framework (Test, testGroup, buildTest)
 import Test.Framework.Providers.HUnit (testCase)
 import Seminal.Compiler.TypeChecker (typecheckModule, TypeCheckStatus (Success, Error), ErrorType (TypeCheckError, ScopeError))
@@ -9,8 +8,13 @@ import Seminal.Compiler.Runner (runCompiler)
 
 runTest :: [FilePath] -> IO TypeCheckStatus
 runTest f = do
-    Right [(_, m)] <- runCompiler f $ parseFiles f
-    runCompiler f $ typecheckModule m
+    res <- runCompiler f (\c -> case c of
+        [(_, m)] -> typecheckModule m
+        _ -> return $ Error (TypeCheckError "")
+        )
+    case res of
+        Right m -> return m
+        Left _ -> assertFailure "Test failed"
 
 testSuite :: Test
 testSuite = testGroup "Compiler's Typechecker" [
