49ddbf535b662c17600df9bdca2c2b0d955ffa33
Arthur Jamet <arthur.jamet@gmail.com>
2023-06-25 16:22:11 +0100

    Enumeration: Move Types into its own file

--- src/Seminal/Enumerator/Signatures.hs
+++ src/Seminal/Enumerator/Signatures.hs
@@ -1,44 +1,11 @@
 module Seminal.Enumerator.Signatures (enumerateChangeInSignature) where
-import GHC (Sig (TypeSig), GhcPs, HsWildCardBndrs (HsWC), GenLocated (L), HsSigType (HsSig), HsType (HsWildCardTy, HsTyVar, HsTupleTy), NoExtField (NoExtField), SrcSpanAnn' (locA), RdrName, EpAnn (EpAnnNotUsed), HsTupleSort (HsBoxedOrConstraintTuple))
+import GHC (Sig (TypeSig), GhcPs, HsWildCardBndrs (HsWC), GenLocated (L), HsSigType (HsSig), SrcSpanAnn' (locA))
 import Seminal.Enumerator.Enumerator (Enumerator)
-import Seminal.Change (ChangeType(..), node, Change (Change), (<&&>))
-import GHC.Plugins (mkRdrUnqual, showPprUnsafe, mkTcOcc)
-import Data.Functor ((<&>))
-import Text.Printf (printf)
+import Seminal.Enumerator.Types(enumerateChangeInType)
+import Seminal.Change ((<&&>))
 
 enumerateChangeInSignature :: Enumerator (Sig GhcPs)
 enumerateChangeInSignature sig _ = case sig of
     (TypeSig xtypesig ids (HsWC xwc (L lbody (HsSig xsig bndrs (L ltype typ))))) -> enumerateChangeInType typ (locA ltype)
         <&&> (TypeSig xtypesig ids. HsWC xwc . L lbody . HsSig xsig bndrs . L ltype)
     _ -> []
-
-enumerateChangeInType :: Enumerator (HsType GhcPs)
-enumerateChangeInType typ loc = [
-    Change (node typ) [node $ HsWildCardTy NoExtField] loc (enumerateChangeInType' typ loc) "The type is incorrect." Wildcard
-    ]
-
-enumerateChangeInType' :: Enumerator (HsType GhcPs)
-enumerateChangeInType' typ loc = case typ of
-    (HsTyVar xvar pflag (L l oldtype)) -> let
-        filteredAtomicTypes = filter (oldtype /=) atomicTypes
-        raisedAtomicTypes = (HsTyVar xvar pflag . L l) <$> filteredAtomicTypes
-        substitutions = unitType:raisedAtomicTypes
-        in substitutions <&> (\newType ->
-            Change (node typ) [node newType] loc []
-            (printf "Expected Type `%s`, got `%s`." (showPprUnsafe newType) (showPprUnsafe oldtype)) Terminal
-            )
-    _  -> []
-
-atomicTypes :: [RdrName]
-atomicTypes = (mkRdrUnqual . mkTcOcc) <$> [
-    "Int",
-    "Integer",
-    "Char",
-    "String",
-    "Bool",
-    "Float",
-    "Double"
-    ]
-
-unitType :: HsType GhcPs
-unitType = HsTupleTy EpAnnNotUsed  HsBoxedOrConstraintTuple []
