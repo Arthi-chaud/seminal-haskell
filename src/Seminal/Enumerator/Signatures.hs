bb97b877ff5f86bc2031e4e5647c083aad22db2c
Arthur Jamet <arthur.jamet@gmail.com>
2023-06-26 10:46:31 +0100

    Change: Define 'forcerewrite

--- src/Seminal/Enumerator/Signatures.hs
+++ src/Seminal/Enumerator/Signatures.hs
@@ -2,10 +2,12 @@ module Seminal.Enumerator.Signatures (enumerateChangeInSignature) where
 import GHC (Sig (TypeSig), GhcPs, HsWildCardBndrs (HsWC), GenLocated (L), HsSigType (HsSig), SrcSpanAnn' (locA))
 import Seminal.Enumerator.Enumerator (Enumerator)
 import Seminal.Enumerator.Types(enumerateChangeInType)
-import Seminal.Change ((<&&>))
+import Seminal.Change ((<&&>), forceRewrite)
+import Data.Functor
 
 enumerateChangeInSignature :: Enumerator (Sig GhcPs)
 enumerateChangeInSignature sig _ = case sig of
     (TypeSig xtypesig ids (HsWC xwc (L lbody (HsSig xsig bndrs (L ltype typ))))) -> enumerateChangeInType typ (locA ltype)
         <&&> (TypeSig xtypesig ids. HsWC xwc . L lbody . HsSig xsig bndrs . L ltype)
+        <&> forceRewrite
     _ -> []
