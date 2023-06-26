54f87dcd39d3ee19c3cd71bac60cfe810e02397d
Arthur Jamet <arthur.jamet@gmail.com>
2023-06-26 09:48:28 +0100

    Type Enumeration: Try to change monads

--- src/Seminal/Enumerator/Types.hs
+++ src/Seminal/Enumerator/Types.hs
@@ -1,8 +1,8 @@
 module Seminal.Enumerator.Types (enumerateChangeInType) where
-import GHC (GhcPs, GenLocated (L), HsType (HsWildCardTy, HsTyVar, HsTupleTy, HsAppTy), NoExtField (NoExtField), RdrName, EpAnn (EpAnnNotUsed), HsTupleSort (HsBoxedOrConstraintTuple))
+import GHC (GhcPs, GenLocated (L), HsType (HsWildCardTy, HsTyVar, HsTupleTy, HsAppTy), NoExtField (NoExtField), RdrName, EpAnn (EpAnnNotUsed), HsTupleSort (HsBoxedOrConstraintTuple), noLocA)
 import Seminal.Enumerator.Enumerator (Enumerator)
 import Seminal.Change (ChangeType(..), node, Change (Change))
-import GHC.Plugins (mkRdrUnqual, showPprUnsafe, mkTcOcc)
+import GHC.Plugins (mkRdrUnqual, showPprUnsafe, mkTcOcc, Outputable (ppr), PromotionFlag (NotPromoted))
 import Data.Functor ((<&>))
 import Text.Printf (printf)
 
@@ -22,10 +22,19 @@ enumerateChangeInType' typ loc = case typ of
             (printf "Expected Type `%s`, got `%s`." (showPprUnsafe newType) (showPprUnsafe oldtype)) Terminal
             )
     -- e.g. Maybe a
-    (HsAppTy _ tparent (L _ tchild)) -> [
-        Change (node typ) [node tchild] loc []
-        (printf "Expected Type `%s`, got `%s`. Maybe you forgot to use `return`?" (showPprUnsafe tparent) (showPprUnsafe tchild)) Terminal
-        ]
+    (HsAppTy x (L lparent tparent) child) -> monadSubstitutions ++ [removeParent]
+        where
+            L _ tchild = child
+            filteredMonads = filter ((ppr tparent /=) . ppr) topMonads
+            monadSubstitutions = buildType <$> filteredMonads <&> (\newM -> Change
+                (node typ) [node $ HsAppTy x (L lparent newM) child] loc []
+                (printf "Expected `%s`, got `%s`." (showPprUnsafe newM) (showPprUnsafe tparent))
+                Terminal
+                )
+            removeParent = Change
+                (node typ) [node tchild] loc []
+                (printf "Expected Type `%s`, got `%s`. Maybe you forgot to use `return`?" (showPprUnsafe tchild) (showPprUnsafe typ))
+                Terminal
     _  -> []
 
 atomicTypes :: [RdrName]
@@ -39,5 +48,14 @@ atomicTypes = (mkRdrUnqual . mkTcOcc) <$> [
     "Double"
     ]
 
+topMonads :: [RdrName]
+topMonads = (mkRdrUnqual . mkTcOcc) <$> [
+    "IO",
+    "Maybe"
+    ]
+
 unitType :: HsType GhcPs
 unitType = HsTupleTy EpAnnNotUsed  HsBoxedOrConstraintTuple []
+
+buildType :: RdrName -> HsType GhcPs
+buildType = HsTyVar EpAnnNotUsed NotPromoted . noLocA
---
+++ test/assets/invalid/signatures/wrong-monad.hs
@@ -0,0 +1,2 @@
+main :: Maybe ()
+main = putStrLn "Hello World"
